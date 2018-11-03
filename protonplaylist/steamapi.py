84e150cbd575f8d72ea2ba68511f3aedc17bffae
190n <benjamin.j.grant@gmail.com>
2018-11-03 11:50:22 -0700

    Move UserGame creation into helper function

--- protonplaylist/steamapi.py
+++ protonplaylist/steamapi.py
@@ -47,6 +47,12 @@ def getUserID(url):
     except IndexError:
         raise Exception('Invalid URL or not a profile URL: {}'.format(url))
 
+# given a dictionary representing a game from getOwnedGames, returns the same information as a UserGame
+def createUserGame(game):
+    # if the Steam API doesn't include playtime_2weeks, the game has not been launched in 2 weeks so the playtime is zero
+    playtimeTwoWeeks = game['playtime_2weeks'] if 'playtime_2weeks' in game else 0
+    return UserGame(game['appid'], game['playtime_forever'], playtimeTwoWeeks)
+
 # given a user ID, return a list of games they own
 def getOwnedGames(userID):
     apiCall = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/'
@@ -59,7 +65,7 @@ def getOwnedGames(userID):
     json = r.json()['response']
     if json == {}:
         raise Exception('Could not get game list')
-    return [UserGame(g['appid'], g['playtime_forever'], g['playtime_2weeks'] if 'playtime_2weeks' in g else 0) for g in json['games']]
+    return [createUserGame(g) for g in json['games']]
 
 # given a list of game IDs, returns various infomration about the games
 def getGameInformation(games):
